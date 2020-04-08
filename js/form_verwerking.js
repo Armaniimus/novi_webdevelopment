c70c792643c1f561a38d63891f381fcc448d79b8
hi <tommy_chester@hotmail.com>
2020-04-08 23:11:45 +0200

    fixed width and form validation

--- js/form_verwerking.js
+++ js/form_verwerking.js
@@ -14,4 +14,51 @@ function GET(VarSearch){
             return KeyValuePair[1];
         }
     }
+
+    return "";
 }
+
+function ValidateNaam() {
+    const naam = GET('naam');
+    const naamEl = document.getElementById('naam');
+
+    if (naam.length > 0) {
+        naamEl.innerHTML = naam;
+        naamEl.classList.add("valid");
+    } else {
+        naamEl.classList.add("invalid");
+    }
+}
+
+function ValidateEmail() {
+    const email = GET('email');
+    const emailEl = document.getElementById('email');
+    const emailRegexPattern = new RegExp( /^.+\@.+\..+$/ );
+
+    if ( emailRegexPattern.test(email) ) {
+        emailEl.classList.add("valid");
+    } else {
+        emailEl.classList.add("invalid");
+    }
+
+    emailEl.innerHTML = email;
+}
+
+function validateGeboorte() {
+    const geboorte = GET('geboorte');
+    const geboorteEl = document.getElementById('geboorte');
+    const geboorteRegExPattern = new RegExp( /^[0-9]{1,4}-[0-9]{1,4}-[0-9]{1,4}$/ );
+
+    if ( geboorteRegExPattern.test(geboorte) ) {
+        geboorteEl.classList.add("valid");
+    } else {
+        geboorteEl.classList.add("invalid");
+    }
+
+
+    geboorteEl.innerHTML = geboorte;
+}
+
+ValidateNaam();
+ValidateEmail();
+validateGeboorte();
